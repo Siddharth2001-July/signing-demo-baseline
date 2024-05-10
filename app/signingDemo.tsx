f7966cea194701afc3a09e4d87978bde4f831d04
Rohitchoudhary-121 <rohit.choudharytechy@gmail.com>
2024-05-10 19:13:41 +0530

    added deleting signees and removed editors from signees

--- app/signingDemo.tsx
+++ app/signingDemo.tsx
@@ -50,7 +50,8 @@ export const SignDemo: React.FC<{ allUsers: User[]; user: User }> = ({
   );
 
   // State to store the current signee i.e the user who is currently selected for which the field will be added
-  const [currSignee, setCurrSignee] = useState<User>(user);
+  //@ts-ignore
+  const [currSignee, setCurrSignee] = useState<User>(users.find((user)=>(user.role!=='Editor')));
   const currSigneeRef = useRef(currSignee);
   currSigneeRef.current = currSignee;
 
@@ -58,7 +59,7 @@ export const SignDemo: React.FC<{ allUsers: User[]; user: User }> = ({
   const [currUser, setCurrUser] = useState<User>(user);
   const currUserRef = useRef(currUser);
   currUserRef.current = currUser;
- 
+
   useEffect(() => {
     setUsers(allUsers);
     setCurrUser(user);
@@ -123,7 +124,7 @@ export const SignDemo: React.FC<{ allUsers: User[]; user: User }> = ({
       top: e.clientY,
       height:
         annotationType === AnnotationTypeEnum.SIGNATURE ||
-        annotationType === AnnotationTypeEnum.INITIAL
+          annotationType === AnnotationTypeEnum.INITIAL
           ? 60
           : 40,
       width: annotationType === AnnotationTypeEnum.INITIAL ? 70 : 120,
@@ -539,6 +540,14 @@ export const SignDemo: React.FC<{ allUsers: User[]; user: User }> = ({
     setCurrSignee(signee);
   };
 
+  const deleteUser = (user: User) => {
+    let remainingUsers = users.filter((userL: User) => (userL.id !== user.id));
+    let currSig = users.find((userL)=>(userL.role!=='Editor'));
+    setUsers(remainingUsers);
+    //@ts-ignore
+    setCurrSignee(currSig);
+  }
+
   return (
     <div>
       <div
@@ -546,6 +555,8 @@ export const SignDemo: React.FC<{ allUsers: User[]; user: User }> = ({
           display: "flex",
           justifyContent: "space-between",
           alignItems: "top",
+          position: 'fixed',
+          width: '100%'
         }}
       >
         <div
@@ -553,23 +564,25 @@ export const SignDemo: React.FC<{ allUsers: User[]; user: User }> = ({
             width: "256px",
             background: "#ffffff",
             borderRight: "1px solid #F0F3F9",
+            overflowY: 'auto',
+            height: '40rem'
           }}
         >
           <div style={{ padding: "16px", borderBottom: "1px solid #D7DCE4" }}>
-            <h3 style={{ 
+            <h3 style={{
               fontFamily: "Inter",
               fontSize: "12px",
               fontWeight: "600",
               lineHeight: "20px",
               textTransform: "uppercase",
-            }}>Current user</h3>
-            <div style={{ 
+            }}>user</h3>
+            <div style={{
               fontFamily: "Inter",
               fontSize: "10px",
               fontWeight: "400",
               lineHeight: "14px",
               marginBottom: "10px"
-            }}>Any text upto 2 lines</div>
+            }}>Currently logged in user</div>
             <Select
               items={users.map((user) => {
                 return {
@@ -592,53 +605,57 @@ export const SignDemo: React.FC<{ allUsers: User[]; user: User }> = ({
           {isVisible && (
             <>
 
-              <div  style={{ padding: "16px", borderBottom: "1px solid #D7DCE4" }}>
-              <h3 style={{ 
-              fontFamily: "Inter",
-              fontSize: "12px",
-              fontWeight: "600",
-              lineHeight: "20px",
-              textTransform: "uppercase",
-            }}>Signees</h3>
-            <div style={{ 
-              fontFamily: "Inter",
-              fontSize: "10px",
-              fontWeight: "400",
-              lineHeight: "14px",
-              marginBottom: "10px"
-            }}>Select a signee for whom you need to add singature</div>
+              <div style={{ padding: "16px", borderBottom: "1px solid #D7DCE4" }}>
+                <h3 style={{
+                  fontFamily: "Inter",
+                  fontSize: "12px",
+                  fontWeight: "600",
+                  lineHeight: "20px",
+                  textTransform: "uppercase",
+                }}>Signees</h3>
+                <div style={{
+                  fontFamily: "Inter",
+                  fontSize: "10px",
+                  fontWeight: "400",
+                  lineHeight: "14px",
+                  marginBottom: "10px"
+                }}>Select a signee for whom you need to add fields</div>
                 <Checkbox
                   label="Ready to sign"
                   isSelected={readyToSign}
                   onChange={(e) => onChangeReadyToSign(e)}
                   style={{ margin: "0px 0px 25px 0px" }}
                 />
-               <div>
                 <div>
-                  {users?.map((user) => (
-                    <div className="heading-custom-style_hover" key={user?.id} onClick= {()=> signeeChanged(user)}>
-                      {user?.name}
-                      <span className="cross">
-                        <svg width="10" height="9" viewBox="0 0 10 9" fill="none" xmlns="http://www.w3.org/2000/svg">
-                          <path d="M4.99991 5.43337L1.73324 8.70003C1.61102 8.82225 1.45547 8.88337 1.26658 8.88337C1.07769 8.88337 0.922133 8.82225 0.79991 8.70003C0.677688 8.57781 0.616577 8.42225 0.616577 8.23337C0.616577 8.04448 0.677688 7.88892 0.79991 7.7667L4.06658 4.50003L0.79991 1.23337C0.677688 1.11114 0.616577 0.955588 0.616577 0.766699C0.616577 0.57781 0.677688 0.422255 0.79991 0.300033C0.922133 0.17781 1.07769 0.116699 1.26658 0.116699C1.45547 0.116699 1.61102 0.17781 1.73324 0.300033L4.99991 3.5667L8.26658 0.300033C8.3888 0.17781 8.54435 0.116699 8.73324 0.116699C8.92213 0.116699 9.07769 0.17781 9.19991 0.300033C9.32213 0.422255 9.38324 0.57781 9.38324 0.766699C9.38324 0.955588 9.32213 1.11114 9.19991 1.23337L5.93324 4.50003L9.19991 7.7667C9.32213 7.88892 9.38324 8.04448 9.38324 8.23337C9.38324 8.42225 9.32213 8.57781 9.19991 8.70003C9.07769 8.82225 8.92213 8.88337 8.73324 8.88337C8.54435 8.88337 8.3888 8.82225 8.26658 8.70003L4.99991 5.43337Z" fill="#EF4444"/>
-                        </svg>
-                      </span>
-                    </div>
-                  ))}
+                  <div>
+                    {users?.map((user) => {
+                      if(user.role!=="Editor")
+                      return (
+                        <div className="heading-custom-style_hover" key={user?.id} onClick={() => signeeChanged(user)}>
+                          <RedCircleIcon color={user.color.toString()} />
+                          {user?.name}
+                          <span className="cross" onClick={() => deleteUser(user)}>
+                            <svg width="10" height="9" viewBox="0 0 10 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+                              <path d="M4.99991 5.43337L1.73324 8.70003C1.61102 8.82225 1.45547 8.88337 1.26658 8.88337C1.07769 8.88337 0.922133 8.82225 0.79991 8.70003C0.677688 8.57781 0.616577 8.42225 0.616577 8.23337C0.616577 8.04448 0.677688 7.88892 0.79991 7.7667L4.06658 4.50003L0.79991 1.23337C0.677688 1.11114 0.616577 0.955588 0.616577 0.766699C0.616577 0.57781 0.677688 0.422255 0.79991 0.300033C0.922133 0.17781 1.07769 0.116699 1.26658 0.116699C1.45547 0.116699 1.61102 0.17781 1.73324 0.300033L4.99991 3.5667L8.26658 0.300033C8.3888 0.17781 8.54435 0.116699 8.73324 0.116699C8.92213 0.116699 9.07769 0.17781 9.19991 0.300033C9.32213 0.422255 9.38324 0.57781 9.38324 0.766699C9.38324 0.955588 9.32213 1.11114 9.19991 1.23337L5.93324 4.50003L9.19991 7.7667C9.32213 7.88892 9.38324 8.04448 9.38324 8.23337C9.38324 8.42225 9.32213 8.57781 9.19991 8.70003C9.07769 8.82225 8.92213 8.88337 8.73324 8.88337C8.54435 8.88337 8.3888 8.82225 8.26658 8.70003L4.99991 5.43337Z" fill="#EF4444" />
+                            </svg>
+                          </span>
+                        </div>
+                      )
+                    })}
+                  </div>
                 </div>
-               </div>
                 <ActionButton
                   label={"+ Add New"}
                   size="md"
                   onPress={addSignee}
                   className="custom-button"
-                  style={{margin: '15px 0px 0px 0px'}}
+                  style={{ margin: '15px 0px 0px 0px' }}
                 />
               </div>
               <Separator variant="secondary" />
               <div style={{ padding: "25px 15px" }}>
                 <h5>Add fields</h5>
-                <p className="para" style={{marginTop: '5px'}}>Add fields by drag & drop them on the document</p>
+                <p className="para" style={{ marginTop: '5px' }}>Add fields by drag & drop them on the document</p>
                 {/* Uncomment this to add draggable name field */}
                 <DraggableAnnotation
                   className="mt-5"
