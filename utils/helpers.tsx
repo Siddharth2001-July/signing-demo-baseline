fa83d918e97e4160d1f6a4cdb65520336f476c8c
Siddharth Kamboj <71596674+Siddharth2001-July@users.noreply.github.com>
2024-05-27 12:34:03 +0530

    signatureUI changes

--- utils/helpers.tsx
+++ utils/helpers.tsx
@@ -5,6 +5,7 @@ const renderConfigurations: any = {};
 export const TOOLBAR_ITEMS = [
   { type: "sidebar-thumbnails" },
   { type: "sidebar-document-outline" },
+  { type: "sidebar-annotations" },
   { type: "pager" },
   { type: "layout-config" },
   { type: "pan" },
@@ -20,7 +21,7 @@ function createCustomSignatureNode({ annotation, type }: any) {
   const container = document.createElement("div");
 
   if (type === AnnotationTypeEnum.SIGNATURE) {
-    container.innerHTML = `<div class="custom-annotation-wrapper custom-signature-wrapper" style="background-color: ${annotation.customData?.signerColor}">
+    container.innerHTML = `<div class="custom-annotation-wrapper custom-signature-wrapper" style="background-color: rgb(${annotation.customData?.signerColor.r},${annotation.customData?.signerColor.g},${annotation.customData?.signerColor.b})">
         <div class="custom-signature">
           <div class="custom-signature-label">
              Sign
@@ -32,7 +33,7 @@ function createCustomSignatureNode({ annotation, type }: any) {
         </div>
       </div>`;
   } else if (type === AnnotationTypeEnum.INITIAL) {
-    container.innerHTML = `<div class="custom-annotation-wrapper custom-signature-wrapper" style="background-color: ${annotation.customData?.signerColor}">
+    container.innerHTML = `<div class="custom-annotation-wrapper custom-signature-wrapper" style="background-color: rgb(${annotation.customData?.signerColor.r},${annotation.customData?.signerColor.g},${annotation.customData?.signerColor.b})">
         <div class="custom-signature">
           <div class="custom-signature-label">
              Initial
@@ -50,7 +51,23 @@ function createCustomSignatureNode({ annotation, type }: any) {
 
 export const getAnnotationRenderers = ({ annotation }: any) => {
   if (annotation.isSignature) {
-    return;
+    // Create a new div element
+  const box = document.createElement('div');
+
+  // Apply box styles
+  box.className = 'signature-box-demo';
+  box.innerHTML = `<span class="signature-label-demo">By PSPDFKit</span><span class="signature-id-demo">${annotation.id.substring(0, 15) + (annotation.id.length > 15 ? '...' : '')}</span>`
+  box.style.height = (annotation.boundingBox.height) + 'px';
+  box.style.width = (annotation.boundingBox.width) + 'px';
+  //box.style.margin = '0px';
+  box.id = annotation.id;
+
+  // Append the annotation to the box
+  //box.appendChild(annotation.node);
+   let ele = { node: box, append: true}
+  // Replace the annotation with the box
+  //annotation.node = box;
+    return ele;
   }
 
   if (annotation.name) {
@@ -93,48 +110,6 @@ export const handleAnnotatitonCreation = async (
             newAnnotation.node.className = "signed";
           }
         }
-
-        // if (
-        //   maybeCorrectAnnotation.customData?.type === AnnotationTypeEnum.DATE &&
-        //   maybeCorrectAnnotation?.customData?.signerEmail === myEmail
-        // ) {
-        //   // save the signDate in the customData of the annotation
-        //   // @ts-ignore
-        //   //const instance = document.pspdfkitInstance;
-        //   const date = formatDateString(Date.now(), "2-digit");
-        //   const annotation = maybeCorrectAnnotation.set("text", {
-        //     format: "plain",
-        //     value: date,
-        //   });
-        //   const dateAnnotationRender = getAnnotationRenderers({
-        //     annotation: maybeCorrectAnnotation,
-        //   });
-        //   if (dateAnnotationRender?.node) {
-        //     const dateAnnotation = dateAnnotationRender.node.querySelector(
-        //       ".custom-annotation-name"
-        //     );
-        //     dateAnnotation.innerHTML = date;
-        //     dateAnnotationRender.node.className =
-        //       dateAnnotationRender.node.className + " signed";
-        //   }
-        //   await instance.update(annotation);
-        //   await instance.save();
-        // } else if (
-        //   maybeCorrectAnnotation.customData?.type === AnnotationTypeEnum.NAME &&
-        //   maybeCorrectAnnotation?.customData?.signerEmail === myEmail
-        // ) {
-        //   //@ts-ignore
-        //   const instance = document.pspdfkitInstance;
-        //   const nameAnnotationRender = getAnnotationRenderers({
-        //     annotation: maybeCorrectAnnotation,
-        //   });
-
-        //   nameAnnotationRender.node.className =
-        //     nameAnnotationRender.node.className + " signed";
-
-        //   await instance.update(maybeCorrectAnnotation);
-        //   await instance.save();
-        // }
       }
     }
     const signatures = [...mySignatureIdsRef.current, annotation.id];
@@ -207,10 +182,12 @@ export const handleAnnotatitonDelete = async (
   }
 };
 
+// SVGs
 export const chatBotSVG = (
   //<svg id="Layer_1" data-name="Layer 1" fill="rgb(245 240 240)" height={50} width={50} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 122.88 119.35"><title>chatbot</title><path d="M57.49,29.2V23.53a14.41,14.41,0,0,1-2-.93A12.18,12.18,0,0,1,50.44,7.5a12.39,12.39,0,0,1,2.64-3.95A12.21,12.21,0,0,1,57,.92,12,12,0,0,1,61.66,0,12.14,12.14,0,0,1,72.88,7.5a12.14,12.14,0,0,1,0,9.27,12.08,12.08,0,0,1-2.64,3.94l-.06.06a12.74,12.74,0,0,1-2.36,1.83,11.26,11.26,0,0,1-2,.93V29.2H94.3a15.47,15.47,0,0,1,15.42,15.43v2.29H115a7.93,7.93,0,0,1,7.9,7.91V73.2A7.93,7.93,0,0,1,115,81.11h-5.25v2.07A15.48,15.48,0,0,1,94.3,98.61H55.23L31.81,118.72a2.58,2.58,0,0,1-3.65-.29,2.63,2.63,0,0,1-.63-1.85l1.25-18h-.21A15.45,15.45,0,0,1,13.16,83.18V81.11H7.91A7.93,7.93,0,0,1,0,73.2V54.83a7.93,7.93,0,0,1,7.9-7.91h5.26v-2.3A15.45,15.45,0,0,1,28.57,29.2H57.49ZM82.74,47.32a9.36,9.36,0,1,1-9.36,9.36,9.36,9.36,0,0,1,9.36-9.36Zm-42.58,0a9.36,9.36,0,1,1-9.36,9.36,9.36,9.36,0,0,1,9.36-9.36Zm6.38,31.36a2.28,2.28,0,0,1-.38-.38,2.18,2.18,0,0,1-.52-1.36,2.21,2.21,0,0,1,.46-1.39,2.4,2.4,0,0,1,.39-.39,3.22,3.22,0,0,1,3.88-.08A22.36,22.36,0,0,0,56,78.32a14.86,14.86,0,0,0,5.47,1A16.18,16.18,0,0,0,67,78.22,25.39,25.39,0,0,0,72.75,75a3.24,3.24,0,0,1,3.89.18,3,3,0,0,1,.37.41,2.22,2.22,0,0,1,.42,1.4,2.33,2.33,0,0,1-.58,1.35,2.29,2.29,0,0,1-.43.38,30.59,30.59,0,0,1-7.33,4,22.28,22.28,0,0,1-7.53,1.43A21.22,21.22,0,0,1,54,82.87a27.78,27.78,0,0,1-7.41-4.16l0,0ZM94.29,34.4H28.57A10.26,10.26,0,0,0,18.35,44.63V83.18A10.26,10.26,0,0,0,28.57,93.41h3.17a2.61,2.61,0,0,1,2.41,2.77l-1,14.58L52.45,94.15a2.56,2.56,0,0,1,1.83-.75h40a10.26,10.26,0,0,0,10.22-10.23V44.62A10.24,10.24,0,0,0,94.29,34.4Z"/></svg>
 <svg style={{marginLeft:'6px'}} fill="white" width={40} height={40} viewBox="0 0 628 628" id="icons" xmlns="http://www.w3.org/2000/svg"><path d="M208,512a24.84,24.84,0,0,1-23.34-16l-39.84-103.6a16.06,16.06,0,0,0-9.19-9.19L32,343.34a25,25,0,0,1,0-46.68l103.6-39.84a16.06,16.06,0,0,0,9.19-9.19L184.66,144a25,25,0,0,1,46.68,0l39.84,103.6a16.06,16.06,0,0,0,9.19,9.19l103,39.63A25.49,25.49,0,0,1,400,320.52a24.82,24.82,0,0,1-16,22.82l-103.6,39.84a16.06,16.06,0,0,0-9.19,9.19L231.34,496A24.84,24.84,0,0,1,208,512Zm66.85-254.84h0Z"/><path d="M400,256a16,16,0,0,1-14.93-10.26l-22.84-59.37a8,8,0,0,0-4.6-4.6l-59.37-22.84a16,16,0,0,1,0-29.86l59.37-22.84a8,8,0,0,0,4.6-4.6L384.9,42.68a16.45,16.45,0,0,1,13.17-10.57,16,16,0,0,1,16.86,10.15l22.84,59.37a8,8,0,0,0,4.6,4.6l59.37,22.84a16,16,0,0,1,0,29.86l-59.37,22.84a8,8,0,0,0-4.6,4.6l-22.84,59.37A16,16,0,0,1,400,256Z"/></svg>
-)
+);
+
 export const downArrowSVG = (
   `<svg
     fill="currentColor"
@@ -228,6 +205,7 @@ export const downArrowSVG = (
     />
   </svg>`
 );
+
 export const upArrowSVG = (
   <svg
     fill="#000000"
@@ -248,6 +226,7 @@ export const upArrowSVG = (
     />
   </svg>
 );
+
 export const initialsSVG = (
   <svg
     xmlns="http://www.w3.org/2000/svg"
@@ -261,6 +240,7 @@ export const initialsSVG = (
     <path d="M0 14h16v2H0v-2zM3.72.74c.79 0 1.42.13 1.91.39s.86.62 1.13 1.1c.26.48.44 1.05.54 1.73.09.67.14 1.42.14 2.25 0 .99-.06 1.85-.18 2.58s-.33 1.33-.62 1.81c-.29.47-.69.83-1.18 1.06-.5.22-1.13.34-1.88.34H0V.74h3.72zm-.44 9.59c.4 0 .72-.07.96-.21.24-.14.43-.36.58-.68.14-.31.23-.73.28-1.24.05-.51.07-1.16.07-1.92 0-.64-.02-1.2-.06-1.69-.04-.49-.13-.89-.26-1.21-.13-.32-.33-.56-.59-.72-.26-.16-.6-.24-1.03-.24h-.96v7.91h1.01zm7.5-1.52c0 .26.02.5.06.72.04.22.12.4.24.54.12.15.27.26.46.35.19.08.44.13.73.13.35 0 .66-.11.94-.34.28-.23.42-.58.42-1.05 0-.25-.03-.47-.1-.65a1.35 1.35 0 0 0-.34-.5c-.16-.15-.37-.28-.62-.4a7.91 7.91 0 0 0-.95-.37c-.5-.17-.94-.35-1.31-.55-.37-.2-.68-.43-.92-.7-.25-.28-.43-.59-.55-.94-.11-.35-.17-.76-.17-1.22 0-1.11.31-1.94.93-2.49.62-.55 1.47-.82 2.55-.82.5 0 .97.05 1.39.17.42.12.79.29 1.1.54.31.25.55.56.73.95.17.38.26.84.26 1.38v.32h-2.17c0-.54-.09-.95-.28-1.24-.19-.29-.5-.43-.95-.43-.25 0-.46.04-.63.11a1.011 1.011 0 0 0-.61.71c-.04.16-.06.32-.06.49 0 .35.07.64.22.87.15.24.46.45.95.65l1.75.76c.43.19.78.39 1.06.59.27.21.49.43.65.66.16.24.28.5.34.78.07.27.1.59.1.93 0 1.19-.34 2.05-1.03 2.59s-1.65.81-2.88.81c-1.28 0-2.2-.28-2.75-.84-.55-.56-.83-1.36-.83-2.4v-.44h2.27v.33z"></path>
   </svg>
 );
+
 export const signSVG = (
   <svg
     xmlns="http://www.w3.org/2000/svg"
@@ -288,6 +268,7 @@ export const personSVG = (
     <path d="M15.31 11.72A4.94 4.94 0 0 0 17 8 5 5 0 0 0 7 8a4.94 4.94 0 0 0 1.69 3.72A8 8 0 0 0 4 19v2h16v-2a8 8 0 0 0-4.69-7.28zM12 4.9A3.1 3.1 0 1 1 8.9 8 3.1 3.1 0 0 1 12 4.9zM6 19a6 6 0 0 1 12 0z"></path>
   </svg>
 );
+
 export const dateSVG = (
   <svg
     xmlns="http://www.w3.org/2000/svg"
